a91807a8e13be3d537de5bd2062fb8215b30675c
aloklearning <alok.kumar@saltpay.co>
2022-11-29 01:02:41 +0000

    Excel to JSON with inputs done

--- excel2json.py
+++ excel2json.py
@@ -22,70 +22,88 @@ import json
 import categories
 import pandas as pd
 
-data_frame = pd.read_excel('assets/Sample-Sheet.xlsx', engine='openpyxl', dtype=object, header=None)
-data_frame = data_frame.fillna("") # Does the job for filling out " " for NaN item (which was a pain)
-feedbacks_array_data = data_frame.values.tolist()
-feedbacks_array_data = feedbacks_array_data[1:len(feedbacks_array_data)]
-
 key_junk = "Junk"
-categories_object = {}
 
-# unique entry logic
-for index, _ in enumerate(feedbacks_array_data):
-    found = False
-    category_item = {}
+def write_data_to_json(final_data_object, save_file_path):
+    with open(f'{save_file_path}/Final-Data.json', 'w', encoding='utf-8') as f:
+        json.dump(final_data_object, f, ensure_ascii=False, indent=4)
+    
+    print("Process success ✅")
+    print(f"We've successfully categorised feedbacks from the provided sheet 🙂")
+    print(f"Please check the Final-Sheet.xlsx file in {save_file_path} directory 📁")
 
-    # 1 -> Project fails when we have something other than string
-    # 2 -> 0th position cosists of the column name, so we don't need it
-    # 3 -> Make sure to have the sheet having English column at 4th pos
-    current_row_data = feedbacks_array_data[index] 
-    feedback = str(current_row_data[4])
-    feedback = feedback.strip()
+def process_unique_entry(array_data, save_file_path):
+    categories_object = {}
 
-    for word in feedback.split():
-        # Skip and move back to line 15 with the next word
-        if word.lower() in categories.ignore_words: continue
+    for index, _ in enumerate(array_data):
+        found = False
+        category_item = {}
 
-        for key in categories.categories:
-            for keyword in categories.categories[key]:
-                if keyword.lower() in word.lower():
-                    found = True
+        # 1 -> Project fails when we have something other than string
+        # 2 -> 0th position cosists of the column name, so we don't need it
+        # 3 -> Make sure to have the sheet having English column at 4th pos
+        current_row_data = array_data[index] 
+        feedback = str(current_row_data[4])
+        feedback = feedback.strip()
 
-                    # Filling the object data and then adding it to the final object
-                    category_item["rating"] = current_row_data[2]
-                    category_item["feedback"] = current_row_data[4].replace("\n", " ")
-                    category_item["method"] = current_row_data[5]
-                    category_item["user_country_code"] = current_row_data[11]
-                    category_item["user_name"] = current_row_data[15]
-                    category_item["user_email"] = current_row_data[16]
+        for word in feedback.split():
+            # Skip and move back to line 15 with the next word
+            if word.lower() in categories.ignore_words: continue
 
-                    if key in categories_object:
-                        if category_item not in categories_object[key]:
-                            categories_object[key].append(category_item)
-                    else:
-                        categories_object[key] = [category_item]
-                    break
-            # Main idea is to push the sentence and move to next sentence immediately
+            for key in categories.categories:
+                for keyword in categories.categories[key]:
+                    if keyword.lower() in word.lower():
+                        found = True
+
+                        # Filling the object data and then adding it to the final object
+                        category_item["rating"] = current_row_data[2]
+                        category_item["feedback"] = current_row_data[4].replace("\n", " ")
+                        category_item["method"] = current_row_data[5]
+                        category_item["user_country_code"] = current_row_data[11]
+                        category_item["user_name"] = current_row_data[15]
+                        category_item["user_email"] = current_row_data[16]
+
+                        if key in categories_object:
+                            if category_item not in categories_object[key]:
+                                categories_object[key].append(category_item)
+                        else:
+                            categories_object[key] = [category_item]
+                        break
+                # Main idea is to push the sentence and move to next sentence immediately
+                if found: break
+            # Breaks word loop
             if found: break
-        # Breaks word loop
-        if found: break
 
-    # Pushing to junk. No word in sentence found in any category
-    if not found:
-        # Filling the object data and then adding it to the final object
-        category_item["rating"] = current_row_data[2]
-        category_item["feedback"] = current_row_data[4].replace("\n", " ")
-        category_item["method"] = current_row_data[5]
-        category_item["user_country_code"] = current_row_data[11]
-        category_item["user_name"] = current_row_data[15]
-        category_item["user_email"] = current_row_data[16]
-        
-        if key_junk in categories_object:
-            if category_item not in categories_object[key_junk]:
-                categories_object[key_junk].append(category_item)
-        else:
-            categories_object[key_junk] = [category_item]
+        # Pushing to junk. No word in sentence found in any category
+        if not found:
+            # Filling the object data and then adding it to the final object
+            category_item["rating"] = current_row_data[2]
+            category_item["feedback"] = current_row_data[4].replace("\n", " ")
+            category_item["method"] = current_row_data[5]
+            category_item["user_country_code"] = current_row_data[11]
+            category_item["user_name"] = current_row_data[15]
+            category_item["user_email"] = current_row_data[16]
+            
+            if key_junk in categories_object:
+                if category_item not in categories_object[key_junk]:
+                    categories_object[key_junk].append(category_item)
+            else:
+                categories_object[key_junk] = [category_item]
 
-# Writing the data to JSON
-with open('Final-Data.json', 'w', encoding='utf-8') as f:
-    json.dump(categories_object, f, ensure_ascii=False, indent=4)
+    write_data_to_json(categories_object, save_file_path)
+
+def process_input_data():
+    user_input_file = input("Please enter the full path of the file along with an xlsx extension: ")
+    save_file_path = input("Please enter the full path where you want to save the result sheet: ")
+    data_frame = pd.read_excel(user_input_file, engine='openpyxl', dtype=object, header=None)
+    data_frame = data_frame.fillna("") # Does the job for filling out " " for NaN item (which was a pain)
+    feedbacks_array_data = data_frame.values.tolist()
+    feedbacks_array_data = feedbacks_array_data[1:len(feedbacks_array_data)]
+
+    print("Work in progress. Have some coffee", "\N{hot beverage}")
+    print(f"Processing {len(feedbacks_array_data)} feedbacks...")
+
+    process_unique_entry(feedbacks_array_data, save_file_path)
+
+# Process starts here
+process_input_data()
